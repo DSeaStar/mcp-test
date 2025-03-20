26cb05382ba47ced3537b0b1f0ff0a936d3e5694
DSeaStar <172368758@qq.com>
2025-03-20 22:28:30 +0800

    feat: 增加本地服务器运行代码

---
+++ output_deobfuscated/Minecraft.java
@@ -0,0 +1,16 @@
+package net.minecraft.client;
+
+import java.io.File;
+
+public class Minecraft {
+    private File mcDataDir;
+    private int displayWidth;
+    
+    public void init() {
+        // 初始化代码
+    }
+    
+    public void run() {
+        // 游戏主循环
+    }
+} 
